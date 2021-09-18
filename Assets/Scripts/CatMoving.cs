353ccb9dd8259ad4edcd66665ce6fc5445774b68
和田萌花 <61648667+momongya@users.noreply.github.com>
2021-09-18 18:08:36 +0900

    ステージの見た目完成
    家設置、猫サイズ変更、ゴールとりあえず置いた

--- Assets/Scripts/CatMoving.cs
+++ Assets/Scripts/CatMoving.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatMoving : MonoBehaviour
 {
 
     int catSpeed = 2;
 
+    //public Transform goal;
+    //NavMeshAgent nav;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -18,5 +22,6 @@ public class CatMoving : MonoBehaviour
     {
         //プレイヤーを動かす
         transform.position += transform.right * catSpeed * Time.deltaTime;
+        //nav.SetDestination(goal.transform.position);
     }
 }

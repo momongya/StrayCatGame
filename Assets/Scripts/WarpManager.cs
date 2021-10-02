5fc6f0206040458d43cbf7dc52d69374e6df3d34
和田萌花 <61648667+momongya@users.noreply.github.com>
2021-10-02 09:23:09 +0900

    warpはできるが丸太が生成されない

--- Assets/Scripts/WarpManager.cs
+++ Assets/Scripts/WarpManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WarpManager : MonoBehaviour
 {
     public GameObject[] flag;
     bool[] flagManage;
-    List<Transform> warpPoint = new List<Transform>();
+    Transform[] warpPoint = new Transform[3];
+
+    Sprite spriteMae;
+    public Sprite spriteAto;
+    public Image warpButton;
 
     // Start is called before the first frame update
     void Start()
@@ -16,37 +21,52 @@ public class WarpManager : MonoBehaviour
         {
             flagManage[i] = false;
         }
+         spriteMae = warpButton.gameObject.GetComponent<Image>().sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckFlagPoint();
-
-        Vector3 mousePos = Input.mousePosition;
-        // Z軸修正
-        mousePos.z = 0f;
     }
 
     void CheckFlagPoint()
     {
         if (transform.position.x >= flag[0].gameObject.transform.position.x && flagManage[0] == false)
         {
-            warpPoint.Add(flag[0].gameObject.transform);
+            warpPoint[0] = flag[0].gameObject.transform;
             flag[0].SetActive(true);
             flagManage[0] = true;
+            warpButton.gameObject.GetComponent<Image>().sprite = spriteAto;
         }
         if (transform.position.x >= flag[1].gameObject.transform.position.x && flagManage[1] == false)
         {
-            warpPoint.Add(flag[1].gameObject.transform);
+            warpPoint[1] = flag[1].gameObject.transform;
             flag[1].SetActive(true);
             flagManage[1] = true;
         }
         if (transform.position.x >= flag[2].gameObject.transform.position.x && flagManage[2] == false)
         {
-            warpPoint.Add(flag[2].gameObject.transform);
+            warpPoint[2] = flag[2].gameObject.transform;
             flag[2].SetActive(true);
             flagManage[2] = true;
+        }
+    }
+
+    public void WarpCat()
+    {
+        int j = -1;
+        for (int i = 0; i < flagManage.Length; i++)
+        {
+            if (flagManage[i] == true)
+            {
+                j = i;
+            }
+        }
+        if (j >= 0)
+        {
+            // 一番近くのチェックポイントにワープ
+            transform.position = warpPoint[j].position;
         }
     }
 }
